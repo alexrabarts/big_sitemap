b5ddea01e0109f681970b82f45330cca001d5b50
Alex Rabarts <alexrabarts@gmail.com>
2009-06-09 11:24:29 +0100

    Gracefully handle the fallback if Rails' polymorphic_url isn't available

--- lib/big_sitemap.rb
+++ lib/big_sitemap.rb
@@ -117,9 +117,8 @@ class BigSitemap
 
               param_method = pick_method(record, PARAM_METHODS)
 
-              location = defined?(Rails) ?
-                polymorphic_url(record) :
-                "#{root_url}/#{strip_leading_slash(options[:path])}/#{record.send(param_method)}"
+              location = defined?(Rails) ? polymorphic_url(record) : nil rescue nil
+              location ||= "#{root_url}/#{strip_leading_slash(options[:path])}/#{record.send(param_method)}"
 
               change_frequency = options[:change_frequency] || 'weekly'
               freq = change_frequency.is_a?(Proc) ? change_frequency.call(record) : change_frequency
